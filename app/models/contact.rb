56fb4fd9d264deaca8a5acb6938cb7e60ea5286e
Omer Fizitsky <omerdp1@gmail.com>
2020-09-05 12:11:58 +0000

    added validation function to contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
-    
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end
+
+# contact.rb checks the database file in order to determine the class's "Contact" parameters.
