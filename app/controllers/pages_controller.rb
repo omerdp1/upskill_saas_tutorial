0e869949aee848a1fd02ac46eba6c163f245ee11
Omer Fizitsky <omerdp1@gmail.com>
2020-09-15 09:32:28 +0000

    added plan vars and parameters to registration paths

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
+  # get request for home page
   def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
+  
+    # get request for about page
+
   def about
   end
 end
